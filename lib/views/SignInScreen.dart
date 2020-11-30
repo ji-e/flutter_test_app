005d57c3b182c821fb6efb0a6170993805960be0
finger <>
2020-11-30 18:08:37 +0900

    로그인 ui ...ing

--- lib/views/SignInScreen.dart
+++ lib/views/SignInScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertestapp/Utils/ColorUtils.dart';
 import 'package:fluttertestapp/provider/SignInProvider.dart';
-import 'package:fluttertestapp/utils/LogUtils.dart';
 import 'package:provider/provider.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -10,7 +10,7 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreen extends State<SignInScreen> {
-   SignInProvider signInProvider;
+  SignInProvider signInProvider;
 
   @override
   void initState() {
@@ -18,33 +18,70 @@ class _SignInScreen extends State<SignInScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: Text('로그인'),
-        ),
-        body: SingleChildScrollView(
-          child: _signInButton(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: ColorUtils.c_ffffff,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text('로그인', style: TextStyle(color: ColorUtils.c_000000)),
       ),
+      body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                  children: <Widget>[_emailEdit(),_signInButton(), ])
+
+//        floatingActionButton: FloatingActionButton(
+//        onPressed:()=>signInProvider.jw1001("jieun1@naver.com"),
+//        tooltip: 'Increment',
+//        child: Icon(Icons.add),
+          )),
     );
   }
 
-  Widget _signInButton(){
-    return GestureDetector(
-      onTap:() async {
+  Widget _signInButton() {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+        height: 60,
+        width: MediaQuery.of(context).size.width,
 
-        final reqData =await signInProvider.jw1001("jieun1@naver.com");
-        var email = reqData.email;
-        LogUtils(StackTrace.current).d(email);
-      },
-      child: Icon(Icons.add),
+        child:  RaisedButton(
+          onPressed: () async {
+            // todo
+            final reqData = signInProvider.jw1001("jieun1@naver.com");
+            print("$reqData" + "FDFDdfdfsdfsf");
+          },
+          shape: RoundedRectangleBorder(borderRadius:  BorderRadius.circular(4)),
+          color: ColorUtils.c_004680,
+          textColor: ColorUtils.c_ffffff,
+          child: Text("계속하기"),
+//          child: Icon(Icons.add),
+        )
     );
   }
 
+  Widget _emailEdit() {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(0, 80, 0, 10),
+        height: 50,
+        child: TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "이메일입력",
+              hintText: "이메일을 입력해주세요"),
+        ));
+  }
 
+//  Widget _pwEdit() {
+//    return TextField(
+//      obscureText: true,
+//      decoration: InputDecoration(
+//          border: OutlineInputBorder(),
+//          labelText: "비밀번호",
+//          hintText: "비밀번호를 입력해주세요"),
+//    );
+//  }
 }
